59522f45513dd8755d3aeb3299c377ba128ee6b1
petpied <elearomy.gros@unibe.ch>
2025-02-28 19:02:40 +0100

    fgjfgjfgj

--- src/processingmm/libmpMuelMat.py
+++ src/processingmm/libmpMuelMat.py
@@ -91,7 +91,7 @@ def _get_CLib_path():
 	# Function to retrieve the global (or local) path to the Compiled C Shared Library
 	'''
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    raise ValueError(os.path.dirname(os.path.realpath(__file__)))
+    raise ValueError(os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'C-libs')))
     # Clibs_pth = os.path.join(dir_path, 'C-libs', 'libmpMuelMat.dll')
     Clibs_pth = os.path.join(dir_path, 'C-libs', 'libmpMuelMat.so')  # << Change the Global (or Local) path here if necessary!
     return Clibs_pth
