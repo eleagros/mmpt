dc2b6aa99d7468ee4b058bb9ff137b2eb9711619
petpied <elearomy.gros@unibe.ch>
2025-03-04 01:32:48 +0100

    changed align_wavelenghts for align_wavelengths

--- tests/align_wls.py
+++ tests/align_wls.py
@@ -7,4 +7,4 @@ directories = os.path.join(os.path.dirname(__file__), "data")
 print('Aligning wavelengths for the database in path: ', directories)
 
 from processingmm.addons import align_wavelengths
-align_wavelengths.align_wavelenghts([directories], 'both', True, [600,650])
+align_wavelengths.align_wavelengths([directories], 'both', True, [600,650])
